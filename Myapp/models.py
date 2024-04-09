afb27377d2f87eb61faeeec417b55970eb270553
EC2 Default User <ec2-user@ip-172-31-45-40.eu-west-1.compute.internal>
2024-04-09 00:08:19 +0000

    Model.py changes

--- Myapp/models.py
+++ Myapp/models.py
@@ -1,10 +1,13 @@
-from django.db import models
+'''module for all the data to be created'''
 import uuid
+from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
-
+#MODEL for al movies data
 class Movie(models.Model):
+    """
+    This class has all the essentail compnents which aare used for basic opeerations
+    """
     title = models.CharField(max_length=255)
     release_date = models.DateField()
     director = models.CharField(max_length=255)
@@ -13,9 +16,15 @@ class Movie(models.Model):
     total_ratings = models.IntegerField(default=0)
     poster = models.FileField(default="leo.jpeg", upload_to="posters")
 
+#model for user to give reeviews and rating
 class Review(models.Model):
+    """
+    This class has all the compnents which are required to 
+    add a review and rating to a specific movie
+    """
     movie = models.ForeignKey(Movie, on_delete=models.CASCADE, related_name='reviews')
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     rating = models.IntegerField(default=0)
     comment = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
+    
